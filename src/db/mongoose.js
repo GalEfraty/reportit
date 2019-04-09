ef88861e986ef5615e42b042e54d7f4cfb5c94a2
Gal Efraty <gal.efraty@gmail.com>
2019-04-10 00:41:16 +0300

    new fixes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,8 @@
 */
 const mongoose = require('mongoose')
 
+//process.env.MONGODB_URL
+//mongodb+srv://reportitAdmin:reportitAdminGalgul@cluster0-vpjmw.mongodb.net/reportit?retryWrites=true
 mongoose.connect(process.env.MONGODB_URL, 
 {
     useNewUrlParser: true,

e2801e619fc17b3d1f234c3471c406f62fd65ea0
Gal Efraty <gal.efraty@gmail.com>
2019-05-14 20:31:11 +0300

    configuration fixes on express

--- src/index.js
+++ src/index.js
@@ -15,10 +15,12 @@ app.use(reportRouter)
 app.use(authorityRouter)
 app.use(scenarioRouter)
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
-  });
+});
 
 app.get('', (req, res) => 
 {
